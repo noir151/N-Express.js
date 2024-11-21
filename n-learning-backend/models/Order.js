b89dfd183f418d60d71e4482f60845b43bcd610c
Noir <149787699+noir151@users.noreply.github.com>
2024-11-21 03:55:05 +0000

    Update Order.js
    Changes made to the model schema to allow more data to be stored

--- n-learning-backend/models/Order.js
+++ n-learning-backend/models/Order.js
@@ -1,27 +1,18 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  lessonId: {
-    type: Number, // Must match a lesson's ID in the lessons collection
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["confirmed", "cancelled"], // Example statuses
-    default: "confirmed",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  lessons: [
+    {
+      lessonId: { type: Number, required: true },
+      quantity: { type: Number, required: true },
+      totalPrice: { type: Number, required: true },
+    },
+  ],
+  status: { type: String, default: "pending" },
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("Order", orderSchema);
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
