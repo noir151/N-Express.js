1db627d8133a4127da96f11301e459881547138d
Noir <149787699+noir151@users.noreply.github.com>
2024-11-29 02:26:01 +0000

    Update lessons.js with commenting

--- n-learning-backend/routes/lessons.js
+++ n-learning-backend/routes/lessons.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Lesson = require('../models/Lesson'); // Import the Lesson model
+const Lesson = require('../models/Lesson'); 
 
-// Route to get all lessons
-router.get('/', async (req, res) => {
+  // Route to get all lessons 
+router.get('/lessons', async (req, res) => {
   try {
-    const lessons = await Lesson.find(); // Fetch all lessons from the database
-    res.json(lessons); // Send the lessons as a response
+    const lessons = await Lesson.find(); // [------ Fetchs all lessons from the database ------]
+    res.json(lessons); // [------ Sends the lessons as a response ------]
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
