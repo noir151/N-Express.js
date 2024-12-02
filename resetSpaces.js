d88b309ba4ef0187ed640bb359b6c9eb7bc6048c
Noir <149787699+noir151@users.noreply.github.com>
2024-12-02 00:45:24 +0000

    Update resetSpaces.js

--- resetSpaces.js
+++ resetSpaces.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const Lesson = require("./models/Lesson"); // Adjust the path to your Lesson model
+const Lesson = require("./models/Lesson"); 
 
 dotenv.config();
 
+//[------ Connects to MongoDB URI ------]
 mongoose
   .connect(process.env.MONGODB_URI) // No options needed for mongoose 6.x and above
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+  //[------ Function to reset the spaces for all lessons in the database ------]
 const resetSpaces = async () => {
   try {
     const result = await Lesson.updateMany({}, { $set: { spaces: 5 } });
@@ -16,7 +18,7 @@ const resetSpaces = async () => {
   } catch (error) {
     console.error("Error resetting spaces:", error);
   } finally {
-    mongoose.disconnect(); // Ensure the connection closes after the script runs
+    mongoose.disconnect();
   }
 };
 
